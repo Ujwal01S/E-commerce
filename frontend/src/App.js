1b57056a3396dba39fe31d4fa67fa276c847231a
Ujwal Suwal <snow7neanderthal477@gmail.com>
2024-07-28 21:49:05 +0545

    Frontend Beggining & Added Bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <>
+      <Layout>
+
+        <h1>E-Commerce</h1>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
