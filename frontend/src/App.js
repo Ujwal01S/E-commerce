75753bd961fda0df45de065cd146f9b9316e9cb6
Ujwal Suwal <snow7neanderthal477@gmail.com>
2024-07-29 09:12:13 +0545

    Frontend navbar and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 
-import './App.css';
-import Layout from './components/Layout/Layout';
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './components/pages/HomePage';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Policy from './components/pages/Policy';
+import PageNotFound from './components/pages/PageNotFound';
 
 function App() {
   return (
     <>
-      <Layout>
-
-        <h1>E-Commerce</h1>
-      </Layout>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/policy' element={<Policy />} />
+        <Route path='*' element={<PageNotFound />} /> {/* (* lae mathi ko sabai route ma error cha vaney yo element ma jau vancha */}
+      </Routes>
     </>
   );
 }
--- frontend/src/components/Layout/Footer.js
+++ frontend/src/components/Layout/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className='bg-dark text-light p-3'>
+    <div className='footer'>
         <h4 className='text-center'>All Right Reserved &copy; Noobie</h4>
+        <p className='footp'>
+        <Link to='/about'>About</Link>|
+        <Link to='/contact'>Contact</Link>|
+        <Link to='/policy'>Policy</Link>
+        </p>
     </div>
   )
 }
