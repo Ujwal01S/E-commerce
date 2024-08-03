a4bead81da75bcd6fdbbd3fa3b15048877548ae8
Ujwal Suwal <snow7neanderthal477@gmail.com>
2024-08-03 09:23:26 +0545

    User and Admin dashboard added including list to functionality

--- frontend/src/pages/Admin/AdminDashboard.js
+++ frontend/src/pages/Admin/AdminDashboard.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout';
+import AdminMenu from '../../components/Layout/AdminMenu';
+import { useAuth } from '../../Context/auth';
+
 const AdminDashboard = () => {
+  const [auth] = useAuth();
   return (
-    <Layout >
-    <h1>AdminDashboard</h1>
+    <Layout title ={'Dashboard - Admin'}>
+      <div className='container-fluid m-3 p-3'>
+        <div className='row'>
+          <div className='col-md-3'>
+            <AdminMenu />
+          </div>
+          <div className='col-md-6'>
+            <div className='card w-75 p-3'>
+              <h3>Admin name: {auth?.user?.name}</h3>
+              <h3>Admin email: {auth?.user?.email}</h3>
+              <h3>Admin contact: {auth?.user?.phone}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
--- frontend/src/pages/user/Dashboard.js
+++ frontend/src/pages/user/Dashboard.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
+import UserMenu from '../../components/Layout/UserMenu'
+import { useAuth } from '../../Context/auth'
 
 const Dashboard = () => {
+  const [auth] = useAuth();
   return (
     <Layout title={'Dashboard - E-commerce App'}>
-        <h1>Dashboard Page</h1>
+        <div className='container-fluid  m-3 p-3'>
+          <div className='row'>
+            <div className='col-md-3'>
+              <UserMenu />
+            </div>
+            <div className='col-md-9'>
+              <h3>{auth?.user?.name}</h3>
+              <h3>{auth?.user?.email}</h3>
+              <h3>{auth?.user?.address}</h3>
+            </div>
+          </div>
+        </div>
 
     </Layout>
   )
